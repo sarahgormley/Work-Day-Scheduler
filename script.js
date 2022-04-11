fd0fb945481fd92eff851b13523f4100cb89ed06
SarahGormley <sarah.gormley39@gmail.com>
2022-04-11 16:04:48 +1000

    setting up localstorage

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 var currentDay = moment();
 $("#currentDay").text(currentDay.format("MMM Do, YYYY"));
+
+var saveBtn = $('.btn')
+var currentTime = $('.hour')
+var planInput = document.querySelector('.description');
+
+planInput.value = localStorage.getItem('content');
+
+function showPlan() {
+    localStorage.setItem('content', planInput.value)
+}
+
+
+
+//On click function for save button
+saveBtn.on('click', showPlan);
